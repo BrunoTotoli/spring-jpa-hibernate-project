ef891ac2dc83e0bf25ee3dee6a3ef12f81e9acce
Bruno Totoli <santosbruno101062@gmail.com>
2022-05-26 15:34:09 -0300

    Payment one to one association

--- src/main/java/com/example/course1/entities/Product.java
+++ src/main/java/com/example/course1/entities/Product.java
@@ -1,7 +1,6 @@
 package com.example.course1.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 import java.io.Serializable;
