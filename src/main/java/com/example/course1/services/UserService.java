7157f858ea42a4e8e974942b7c639818b6ffe4d2
Bruno Totoli <santosbruno101062@gmail.com>
2022-06-04 16:44:04 -0300

    User update

--- src/main/java/com/example/course1/services/UserService.java
+++ src/main/java/com/example/course1/services/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
         repository.deleteById(id);
     }
 
+    public User update(Long id, User user) {
+        User entity = repository.getOne(id);
+        updateData(entity, user);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User user) {
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setPhone(user.getPhone());
+    }
+
 }
